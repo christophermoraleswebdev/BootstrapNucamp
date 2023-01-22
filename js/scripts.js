3cfc3299545e91f28aa1be0c342d7310650afd99
Christopher Morales <christopher.moralesadj@gmail.com>
2023-01-21 22:29:59 -0600

    added jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,16 @@ $(function() {
         }
     });
 });
+
+        
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $("#reserveModal").modal('show');
+    });
+});
+
+$(document).ready(function(){
+    $("#loginButton").click(function(){
+        $("#loginModal").modal('show');
+    });
+});
